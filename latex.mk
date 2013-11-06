5675504254d88e1fc99023646685e80867b1d1bc
j8takagi <j8takagi@nifty.com>
2013-11-06 23:56:36 +0900

    latex.mkを更新

--- latex.mk
+++ latex.mk
@@ -1,6 +1,39 @@
-.PHONY: clean-tex distclean-tex
+# latex.mk
+# LaTeX処理（コンパイル）を行う
+#
+# == 使い方 ==
+# 1. texソースファイルと同じディレクトリーに本ファイル（latex.mk）をコピーする
+# 2. Makefileに変数TARGETS と「include latex.mk」を記述する
+# 3. texソースファイルと同じディレクトリーで、make コマンドを実行する
+#
+# == 機能 ==
+# - 読み込むべき中間ファイルがないことや相互参照未定義の警告がある場合、LaTeX処理を最大4回繰り返す
+# - \includegraphics命令がTeXファイルに含まれる場合、グラフィックファイルを挿入
+#   -- 挿入されたグラフィックファイルが更新されたときは、処理を開始
+#   -- 挿入されたグラフィックファイルがないときは、処理を中止
+#   -- 挿入されたグラフィックファイルに対するバウンディング情報ファイル（.xbb）を作成
+# - \include、\input命令がTeXファイルに含まれる場合、TeXファイルを挿入
+#   -- 挿入されたTeXファイルが更新されたときは、処理を開始
+#   -- 挿入されたTeXファイルがないときは、処理を中止
+# - \makeindex命令が含まれる場合、mendexで索引を作成
+# - \bibliography命令が含まれる場合、BiBTeXで文献一覧を作成
+
+# == 擬似ターゲット ==
+# - tex-clean: TeX中間ファイル（auxなど）を削除。ターゲットに.dviが含まれていないときは.dviファイルを削除
+# - xbb-clean: バウンディング情報ファイル（.xbb）を削除
+# - tex-distclean: TeX中間ファイル、バウンディング情報ファイル、ターゲットファイル（PDF、.dvi）を削除
+#
+# === Makefile -- sample ===
+# TARGETS := report.tex
+#
+# all: $(TARGETS)
+#
+# include latex.mk
+
+.PHONY: tex-clean tex-distclean
 
 # シェルコマンド
+CAT := cat
 CMP := cmp -s
 CP := cp
 ECHO := echo
@@ -14,8 +47,51 @@ EXTRACTBB := extractbb
 BIBTEX := pbibtex
 MENDEX := mendex
 
+# TeX中間ファイルの拡張子
+#   .aux: 相互参照用
+#   .fls: tex -recorderで生成されるファイルリスト
+#   .lof: 図リスト（\tableoffigures）用
+#   .lot: 表リスト（\tableoftables）用
+#   .out: hyperrefパッケージ用
+#   .toc: 目次（\tableofcontents）用
+#   .log: ログ
+TEX_INT := .aux .fls .lof .lot .out .toc .log
+
+# 索引中間ファイルの拡張子
+#   .idx: auxから作成
+#   .ind: idxから作成
+#   .ilg: 索引ログ
+IND_INT := .idx .ind .ilg
+
+# BiBTeX中間ファイルの拡張子
+#   .bbl: auxから作成
+#   .blg: BiBTeXログ
+BIB_INT := .bbl .blg
+
+# ファイル名から拡張子を除いた部分
+FILEBASE = $(basename $<)
+# .texファイル
+TEXFILE = $(addsuffix .tex,$(FILEBASE))
+# .auxファイル
+AUXFILE = $(addsuffix .aux,$(FILEBASE))
+# .prev_auxファイル。.auxファイルのコピー
+PREV_AUXFILE = $(addsuffix .prev_aux,$(FILEBASE))
+# .dviファイル
+DVIFILE = $(addsuffix .dvi,$(FILEBASE))
+# .dファイル
+DFILE = $(addsuffix .d,$(FILEBASE))
+# .logファイル
+LOGFILE = $(addsuffix .log,$(FILEBASE))
+# .idxファイル
+IDXFILE = $(addsuffix .idx,$(FILEBASE))
+# .prev_idxファイル。.idxファイルのコピー
+PREV_IDXFILE = $(addsuffix .prev_idx,$(FILEBASE))
+# .indファイル
+INDFILE = $(addsuffix .ind,$(FILEBASE))
+# .bblファイル
+BBLFILE = $(addsuffix .bbl,$(FILEBASE))
+
 #LaTeXオプション
-interaction ?= nonstopmode
 LATEXFLAG ?=
 DVIPDFMXFLAG ?=
 EXTRACTBBFLAGS ?=
@@ -23,92 +99,86 @@ BIBTEXFLAG ?=
 MENDEXFLAG ?=
 
 # LaTeX処理（コンパイル）
-COMPILE.tex = $(LATEX) -interaction=$(interaction) $(LATEXFLAG) $(addsuffix .tex,$(basename $<))
+COMPILE.tex = $(LATEX) -interaction=batchmode $(LATEXFLAG) $(TEXFILE) || $(CAT) $(LOGFILE)
 
-# 相互参照の警告
-WARN_REF := 'Label(s) may have changed. Rerun to get cross-references right.'
+# 相互参照未定義の警告
+WARN_UNDEFREF := 'There were undefined references\.'
 
-# 相互参照未定義の警告。2回目以降の処理で出る場合は、参照エラー
-WARN_UNDEFREF := 'There were undefined references.'
+# 読み込むべき中間ファイルがないことの警告
+WARN_NOFILE = 'No file $(FILEBASE)\.[a-zA-Z0-9]*\.'
 
-# auxやtocなどのファイルがない警告
-WARN_NOFILE := 'No file'
-
-# LaTeX処理の最大回数
-MAX_CNT := 10
-
-# logファイルに相互参照または目次ファイルなしの警告がある場合、LaTeX処理を繰り返す
-# 2回目以降の処理で相互参照未定義の警告がある場合と、
-# 繰り返しの回数がMAX_CNTになった場合は、警告を表示してエラー終了
+# ログファイルに警告がある場合、LaTeX処理を最大4回繰り返す
 COMPILES.tex = \
-  cnt=0; \
-  while $(GREP) -F -e $(WARN_REF) -e $(WARN_NOFILE) $(addsuffix .log,$(basename $<)); do \
-    if test $$cnt -ge $(MAX_CNT); then \
-      $(ECHO) "LaTeX compile is over $$cnt times, but warnings exist."; \
-      exit 1; \
-    fi; \
+  if test -s $(INDFILE); then \
+    $(ECHO) "---------- for index ----------"; \
     $(COMPILE.tex); \
-    if test $$cnt -eq 1 && $(GREP) -F $(WARN_UNDEFREF) $(addsuffix .log,$(basename $<)); then \
-      exit 1; \
+  fi; \
+  for f in 1st 2nd 3rd final; do \
+    if test -s $@ -a -s $(LOGFILE); then \
+      $(GREP) -e $(WARN_UNDEFREF) -e $(WARN_NOFILE) $(LOGFILE) || exit 0; \
     fi; \
-    cnt=`expr $$cnt + 1`; \
+    $(ECHO) "---------- $$f try ----------"; \
+    $(COMPILE.tex); \
   done
 
-# \includeコマンドで読み込まれるtexファイル
-intex = $(addsuffix .tex,$(basename $(strip $(shell \
-  $(SED) -n -e 's/\\\(include\|input\)\(\[[^]]*\]\)\{0,1\}{[^}]*}/&\n/gp' $<  | \
-  $(SED) -e 's/.*{\([^}]*\)}.*/\1/'))))
-
-# \includegraphicsコマンドで読み込まれるグラフィックファイル
+# \includegraphics命令で読み込まれるグラフィックファイル
 ingraphics = $(strip $(shell \
-  $(SED) -n -e 's/\\includegraphics\(\[[^]]*\]\)\{0,1\}\({[^}]*}\)/&\n/gp' $< $(intex) | \
+  $(SED) -n -e '/^%/!s/\\includegraphics\(\[[^]]*\]\)\{0,1\}\({[^}]*}\)/&\n/gp' $< $(intex) | \
   $(SED) -e 's/.*{\([^}]*\)}.*/\1/'))
 
-# \bibliographyコマンドで読み込まれる文献データベースファイル
+# \include、\input命令で読み込まれるtexファイル
+intex = $(addsuffix .tex,$(basename $(strip $(shell \
+  $(SED) -n -e '/^%/!s/\\\(include\|input\)\(\[[^]]*\]\)\{0,1\}{[^}]*}/&\n/gp' $< | \
+  $(SED) -e 's/.*{\([^}]*\)}.*/\1/'))))
+
+# \makeindex命令
+makeindex = $(SED) -n -e '/^%/!s/\\makeindex/&/p' $<
+
+# \bibliography命令で読み込まれる文献データベースファイル
 bibdb = $(addsuffix .bib,$(basename $(strip $(shell \
-  $(SED) -n -e 's/\\bibliography\(\[[^]]*\]\)\{0,1\}{\([^}]*\)}/&\n/gp' $< $(intex) | \
+  $(SED) -n -e '/^%/!s/\\bibliography\(\[[^]]*\]\)\{0,1\}{\([^}]*\)}/&\n/gp' $< $(intex) | \
   $(SED) -e 's/.*{\([^}]*\)}.*/\1/' | \
   $(SED) -e 's/,/ /g'))))
 
-# texファイルに索引作成の指定があるか確認
-MAKEINDEX := '\makeindex'
-GREP-makeindex = $(GREP) -F $(MAKEINDEX) $<
-
-# 依存関係を自動生成し、dファイルに格納
+# 依存関係を.dファイルに書き出す
 %.d: %.tex
 	@$(ECHO) '$@ is created by scanning $^.'
   # texファイルの依存関係
-	@($(ECHO) '$(subst .tex,.dvi,$<) $(subst .tex,.aux,$<) $(subst .tex,.d,$<): $<' >$@; \
-      $(ECHO) >>$@; \
-      $(ECHO) '$(subst .tex,.prev_aux,$<):' >>$@)
-  # Include/Inputファイルの依存関係
-	$(if $(intex),@( \
+	@($(ECHO) '$(AUXFILE) $(DFILE): $<'; \
       $(ECHO); \
-      $(ECHO) '# Include/Input Files - tex'; \
-      $(ECHO) '$(subst .tex,.dvi,$<) $(subst .tex,.aux,$<): $(intex)') >>$@)
+      $(ECHO) '$(DVIFILE): $(AUXFILE)') >$@
   # 画像ファイルの依存関係
 	$(if $(ingraphics),@( \
       $(ECHO); \
-      $(ECHO) '# IncludeGraphic Files - pdf, jpeg/jpg, png & xbb'; \
-      $(ECHO) '$(subst .tex,.dvi,$<) $(subst .tex,.aux,$<): $(ingraphics)'; \
+      $(ECHO) '# IncludeGraphic Files - .pdf, .jpeg/.jpg, .png with .xbb'; \
+      $(ECHO) '$(DVIFILE) $(AUXFILE): $(ingraphics)'; \
       $(ECHO); \
-      $(ECHO) '$(subst .tex,.dvi,$<) $(subst .tex,.aux,$<): $(addsuffix .xbb,$(basename $(ingraphics)))') >>$@)
+      $(ECHO) '$(DVIFILE) $(AUXFILE): $(addsuffix .xbb,$(basename $(ingraphics)))') >>$@)
+  # Include/Inputファイルの依存関係
+	$(if $(intex),@( \
+      $(ECHO); \
+      $(ECHO) '# Include / Input Files - .tex'; \
+      $(ECHO) '$(DVIFILE) $(AUXFILE): $(intex)') >>$@)
+  # 索引作成用ファイルの依存関係
+	$(if $(makeindex),@( \
+      $(ECHO); \
+      $(ECHO) '# Index Files: .aux -> idx -> .ind -> .dvi'; \
+      $(ECHO) '$(IDXFILE): $<'; \
+      $(ECHO); \
+      $(ECHO) '$(PREV_IDXFILE): $(IDXFILE)'; \
+      $(ECHO); \
+      $(ECHO) '$(INDFILE): $(PREV_IDXFILE)'; \
+      $(ECHO); \
+      $(ECHO) '$(DVIFILE): $(INDFILE)') >>$@)
   # 文献処理用ファイルの依存関係
 	$(if $(bibdb),@( \
-       $(ECHO); \
-       $(ECHO) '# Bibliography Files - bbl & bib'; \
-       $(ECHO) '$(subst .tex,.dvi,$<): $(subst .tex,.bbl,$<)'; \
-       $(ECHO); \
-       $(ECHO) '$(subst .tex,.bbl,$<): $(bibdb)') >>$@)
-  # 索引作成用ファイルの依存関係
-	$(if $(strip $(shell $(GREP-makeindex))),@( \
-       $(ECHO); \
-       $(ECHO) '# MakeIndex Files - ind'; \
-       $(ECHO) '$(subst .tex,.idx,$<):'; \
-       $(ECHO); \
-       $(ECHO) '$(subst .tex,.ind,$<):'; \
-       $(ECHO); \
-       $(ECHO) '$(subst .tex,.dvi,$<): $(subst .tex,.ind,$<)') >>$@)
+      $(ECHO); \
+      $(ECHO) '# Bibliography Files - .bbl, .bib'; \
+      $(ECHO) '$(PREV_AUXFILE): $(AUXFILE)'; \
+      $(ECHO); \
+      $(ECHO) '$(DVIFILE): $(BBLFILE)'; \
+      $(ECHO); \
+      $(ECHO) '$(BBLFILE): $(bibdb)') >>$@)
 
 # 変数TARGETSで指定されたターゲットファイルに対応する
 # .dファイルをインクルードし、依存関係を取得する
@@ -117,24 +187,22 @@ ifeq (,$(filter %clean %.xbb %.d,$(MAKECMDGOALS)))
   -include $(addsuffix .d,$(basename $(TARGETS)))
 endif
 
-# 相互参照ファイル作成。dviファイルも同時に作成される
+# dviファイル作成
+%.dvi: %.tex
+	$(COMPILES.tex)
+
+# auxファイル作成
 %.aux: %.tex
 	$(COMPILE.tex)
 
 %.prev_aux: %.aux
 	-$(CMP) $@ $< || $(CP) $< $@
 
-# dviファイル作成
-%.dvi: %.aux
-	$(COMPILES.tex)
-
-# 文献処理用ファイル作成
-# BiBTeXで文献処理するときに使用される
-%.bbl: %.prev_aux
-	$(BIBTEX) $(BIBTEXFLAG) $(subst .prev_aux,.aux,$<)
+# PDFファイル作成
+%.pdf: %.dvi
+	$(DVIPDFMX) $(DVIPDFMXFLAG) $<
 
 # バウンディング情報ファイル作成
-# dvipdfmxで図を挿入するときに使用される
 # pdf、jpeg/jpg、pngファイルに対応
 %.xbb: %.pdf
 	$(EXTRACTBB) $(EXTRACTBBFLAGS) $<
@@ -149,24 +217,33 @@ endif
 	$(EXTRACTBB) $(EXTRACTBBFLAGS) $<
 
 # 索引ファイル作成
-# 索引を作成するときに使用される
-%.idx: %.aux
+%.idx: %.tex
 	$(COMPILE.tex)
 
 %.prev_idx: %.idx
 	-$(CMP) $@ $< || $(CP) $< $@
 
 %.ind: %.prev_idx
-	$(MENDEX) $(MENDEXFLAG) $(subst .prev_idx,.idx,$<)
+	$(MENDEX) $(MENDEXFLAG) $(IDXFILE)
 
-# PDFファイル作成
-%.pdf: %.dvi
-	$(DVIPDFMX) $(DVIPDFMXFLAG) $<
+# 文献処理用ファイル作成
+%.bbl: %.prev_aux
+	$(BIBTEX) $(BIBTEXFLAG) $(AUXFILE)
 
 # tex-cleanターゲット
 tex-clean:
-	$(RM) *.aux *.bbl *.blg *.idx *.ilg *.ind *.lof *.lot *.out *.toc *.xbb *.log *.d
+	$(RM) $(addprefix *,$(TEX_INT) $(IND_INT) $(BIB_INT) .d .prev_aux .prev_idx)
+ifeq (,$(filter %.dvi,$(TARGETS)))
+	$(RM) *.dvi
+endif
+
+# xbb-cleanターゲット
+xbb-clean:
+	$(RM) *.xbb
 
 # tex-distcleanターゲット
-tex-distclean: tex-clean
-	$(RM) $(addsuffix .dvi,$(basename $(TARGETS))) $(addsuffix .pdf,$(basename $(TARGETS)))
+tex-distclean: tex-clean xbb-clean
+ifneq (,$(filter %.dvi,$(TARGETS)))
+	$(RM) *.dvi
+endif
+	$(RM) $(TARGETS)
